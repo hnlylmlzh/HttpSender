8ad12c85cebe6c4a5b4dae0a62a935050c165f10
bingyangleng <lbynew@163.com>
2018-08-20 21:18:55 +0800

    update

---
+++ src/Http.client.test/SenderTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+using Http.client;
+
+namespace Http.client.test
+{
+    public class SenderTest
+    {
+        [Fact]
+        public void GetTest()
+        {
+            string getResult = Sender.Get("http://localhost:5000/api/values/1");
+            Assert.Equal("value",getResult);
+        }
+    }
+}
---
+++ src/Http.client/Sender.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+
+namespace Http.client
+{
+    public class Sender
+    {
+        private static HttpClient client = new HttpClient();
+
+        public static string Get(string url)
+        {
+            HttpResponseMessage result = client.GetAsync(url).Result;
+            return result.Content.ReadAsStringAsync().Result;
+        }
+    }
+}
