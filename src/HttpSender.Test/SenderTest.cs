15408e472c6d4a239c34b6e9d19487551a8aed57
bingyangleng <lbynew@163.com>
2018-11-23 20:58:07 +0800

    update

--- src/HttpSender.Test/SenderTest.cs
+++ src/HttpSender.Test/SenderTest.cs
@@ -10,35 +10,35 @@ namespace HttpSender.Test
         [Fact]
         public void GetTest()
         {
-            string getResult = Sender.Get("http://localhost:5000/home/about?a=1");
+            string getResult = Sender.Get("http://localhost/mvcserver/home/about?a=1");
             Assert.Equal("Get 1",getResult);
-            getResult = Sender.Get("http://localhost:6000/api/values");
+            getResult = Sender.Get("http://localhost/apiserver/api/values");
             Assert.Equal("[\"value1\",\"value2\"]", getResult);
         }
 
         [Fact]
         public void PostTest()
         {
-            string postResult = Sender.Post("http://localhost:5000/home/contact", "b=2");
+            string postResult = Sender.Post("http://localhost/mvcserver/home/contact", "b=2");
             Assert.Equal("Post 2", postResult);
-            postResult = Sender.Post("http://localhost:6000/api/values", "value=asd");
-            Assert.Equal("asd", postResult);
-            postResult = Sender.Post("http://localhost:6000/api/values", "value=汉字");
-            Assert.Equal("汉字", postResult);
+            postResult = Sender.Post("http://localhost/apiserver/api/values", "value=asd");
+            Assert.Equal("\"asd\"", postResult);
+            postResult = Sender.Post("http://localhost/apiserver/api/values", "value=汉字");
+            Assert.Equal("\"汉字\"", postResult);
         }
 
         [Fact]
         public void PostDictionaryTest()
         {
             Dictionary<string, string> test = new Dictionary<string, string> { { "b", "3" } };
-            string postResult = Sender.Post("http://localhost:5000/home/contact", test);
+            string postResult = Sender.Post("http://localhost/mvcserver/home/contact", test);
             Assert.Equal("Post 3", postResult);
         }
 
         [Fact]
         public void PutUrlTest()
         {
-            string putResult = Sender.Put("http://localhost:5000/home/testput?c=4");
+            string putResult = Sender.Put("http://localhost/mvcserver/home/testput?c=4");
             Assert.Equal("Put 4", putResult);
         }
 
@@ -46,35 +46,35 @@ namespace HttpSender.Test
         public void PutTest()
         {
             Dictionary<string, string> test = new Dictionary<string, string> { { "c", "5" } };
-            string putResult = Sender.Put("http://localhost:5000/home/testput", test);
+            string putResult = Sender.Put("http://localhost/mvcserver/home/testput", test);
             Assert.Equal("Put 5", putResult);
         }
 
         [Fact]
         public void DeleteTest()
         {
-            string deleteResult = Sender.Delete("http://localhost:5000/home/testdelete?d=6");
+            string deleteResult = Sender.Delete("http://localhost/mvcserver/home/testdelete?d=6");
             Assert.Equal("Delete 6", deleteResult);
         }
 
         [Fact]
         public void WaitTest()
         {
-            Assert.Throws<AggregateException>(()=>Sender.Get("http://localhost:5000/home/wait?a=1"));
+            Assert.Throws<AggregateException>(()=>Sender.Get("http://localhost/mvcserver/home/wait?a=1"));
         }
 
         [Fact]
         public void AuthTest()
         {
-            string result = Sender.Get("http://localhost:5000/home/testauth?e=7");
+            string result = Sender.Get("http://localhost/mvcserver/home/testauth?e=7");
             Assert.NotEqual("Auth 7", result);
             Sender.OAuth("test");
-            result = Sender.Get("http://localhost:5000/home/testauth?e=7");
+            result = Sender.Get("http://localhost/mvcserver/home/testauth?e=7");
             Assert.Equal("Auth 7", result);
-            result = Sender.Get("http://localhost:5000/home/testauth?e=8");
+            result = Sender.Get("http://localhost/mvcserver/home/testauth?e=8");
             Assert.Equal("Auth 8", result);
             Sender.OAuth("test1");
-            result = Sender.Get("http://localhost:5000/home/testauth?e=7");
+            result = Sender.Get("http://localhost/mvcserver/home/testauth?e=7");
             Assert.NotEqual("Auth 7", result);
         }
 
@@ -82,27 +82,27 @@ namespace HttpSender.Test
         public void DemoTest()
         {
             string Response = string.Empty;
-            Response = Sender.Get("http://localhost:5000/home/info?username=jim");
+            Response = Sender.Get("http://localhost/mvcserver/home/info?username=jim");
             Assert.Equal("Hello, jim",Response);
-            Response = Sender.Post("http://localhost:5000/home/login", "username=jim&password=123456");
+            Response = Sender.Post("http://localhost/mvcserver/home/login", "username=jim&password=123456");
             Assert.Equal("true", Response.ToLower());
             Dictionary<string, string> LoginInfo = new Dictionary<string, string>
             {
               { "username", "jim" },
               { "password", "123456" }
             };
-            Response = Sender.Post("http://localhost:5000/home/login", LoginInfo);
+            Response = Sender.Post("http://localhost/mvcserver/home/login", LoginInfo);
             Assert.Equal("true", Response.ToLower());
-            Response = Sender.Put("http://localhost:5000/home/update?username=jim&age=15");
+            Response = Sender.Put("http://localhost/mvcserver/home/update?username=jim&age=15");
             Assert.Equal("jim's age is 15",Response);
             Dictionary<string, string> UpdateInfo = new Dictionary<string, string>
             {
                 { "username", "jim" },
                 { "age" , "15"}
             };
-            Response = Sender.Put("http://localhost:5000/home/update", UpdateInfo);
+            Response = Sender.Put("http://localhost/mvcserver/home/update", UpdateInfo);
             Assert.Equal("jim's age is 15", Response);
-            Response = Sender.Delete("http://localhost:5000/home/delete?username=jim&year=2011");
+            Response = Sender.Delete("http://localhost/mvcserver/home/delete?username=jim&year=2011");
             Assert.Equal("jim's information in 2011 has been deleted",Response);
         }
     }
