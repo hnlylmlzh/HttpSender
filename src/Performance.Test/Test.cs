24c9d9f81b7fd05b1520d337b514b58910225bc1
bingyangleng <lbynew@163.com>
2018-11-23 21:23:28 +0800

    update

--- src/Performance.Test/Test.cs
+++ src/Performance.Test/Test.cs
@@ -12,22 +12,22 @@ namespace Performance.Test
         public void start()
         {
             string Response = string.Empty;
-            Response = Sender.Get("http://localhost:5000/home/info?username=jim");
-            Response = Sender.Post("http://localhost:5000/home/login", "username=jim&password=123456");
+            Response = Sender.Get("http://localhost/mvcserver/home/info?username=jim");
+            Response = Sender.Post("http://localhost/mvcserver/home/login", "username=jim&password=123456");
             Dictionary<string, string> LoginInfo = new Dictionary<string, string>
             {
               { "username", "jim" },
               { "password", "123456" }
             };
-            Response = Sender.Post("http://localhost:5000/home/login", LoginInfo);
-            Response = Sender.Put("http://localhost:5000/home/update?username=jim&age=15");
+            Response = Sender.Post("http://localhost/mvcserver/home/login", LoginInfo);
+            Response = Sender.Put("http://localhost/mvcserver/home/update?username=jim&age=15");
             Dictionary<string, string> UpdateInfo = new Dictionary<string, string>
             {
                 { "username", "jim" },
                 { "age" , "15"}
             };
-            Response = Sender.Put("http://localhost:5000/home/update", UpdateInfo);
-            Response = Sender.Delete("http://localhost:5000/home/delete?username=jim&year=2011");
+            Response = Sender.Put("http://localhost/mvcserver/home/update", UpdateInfo);
+            Response = Sender.Delete("http://localhost/mvcserver/home/delete?username=jim&year=2011");
         }
     }
 }
